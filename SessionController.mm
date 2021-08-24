2969e5bcca12938948cbcd8db307d63ad3ae210e
semvis123 <semvisscher10@gmail.com>
2021-08-24 21:30:08 +0200

    🐛  fix weird bug when writing data to EAAcessory

--- SessionController.mm
+++ SessionController.mm
@@ -18,7 +18,7 @@ NSString *SessionDataReceivedNotification = @"SessionDataReceivedNotification";
 			NSLog(@"write error");
 			break;
 		} else if (bytesWritten > 0) {
-			 [_writeData replaceBytesInRange:NSMakeRange(0, fmin(bytesWritten, [_writeData length])) withBytes:NULL length:fmax([_writeData length] - bytesWritten, 0)];
+			 [_writeData replaceBytesInRange:NSMakeRange(0, [_writeData length]) withBytes:NULL length:0];
 		}
 	}
 	[_writeDataCondition signal];
